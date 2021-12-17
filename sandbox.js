dff3e25b3b9418a30cb2d0d1047718c2ad43ff0d
Shapati <93589375+Shapati@users.noreply.github.com>
2021-12-17 23:18:18 +0100

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,17 @@
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('#nav');
+const menubg = document.querySelector('.menu-bg')
+menu.addEventListener('click',()=>{
+    menu.classList.toggle('change')
+    nav.classList.toggle('change')
+    menubg.classList.toggle('change-bg')
+    
+})
+
+const img = document.querySelector
+
+
+
+// function onClickMenu(){
+// document.getElementById("nav").classList.toggle('change')
+// }
